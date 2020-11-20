eabc522d930d3ce4dd602f87d84e4456b43a2a61
Travis Sanders <trav8437@gmail.com>
2020-11-19 21:16:54 -0700

    End Unit 2

--- js-in-the-dom/index.js
+++ js-in-the-dom/index.js
@@ -1,1 +1,20 @@
+var nameInput
 
+
+var emailInput
+
+let validateForm = function(){
+
+}
+
+let removeMessage = function(){
+
+}
+
+let displayThankYou = function(){
+
+}
+
+let addToCart = function(){
+    
+} 
